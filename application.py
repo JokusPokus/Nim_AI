6f6208d2e702fdccfbd7c034c7503a0e360a74b2
JokusPokus <jakob.schmitt@code.berlin>
2020-05-02 18:06:12 +0200

    add and import AI logic

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from nimAI import train, play
 
 app = Flask(__name__)
 
@@ -8,6 +9,10 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/nim")
+@app.route("/nim", methods=["GET", "POST"])
 def nim():
-    return render_template("nim.html")
+    if request.method == "GET":
+        word = None
+    else:
+        word = request.form.get("word")
+    return render_template("nim.html", word=word)
